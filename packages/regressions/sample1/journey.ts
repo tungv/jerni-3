a66de134ad3c17baf780ea405bab4104f346defe
Tung Vu <me@tungv.com>
2024-01-30 15:02:04 +0700

    test: add regression tests

---
+++ packages/regressions/sample1/journey.ts
@@ -0,0 +1,38 @@
+import createJourney from "jerni";
+import { makeMongoDBStore } from "@jerni/store-mongodb";
+
+import { LicenseeModel } from "./licensee";
+
+export default async function initJourney() {
+  const mongodbUrl = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017";
+  const mongodbDatabase = process.env.MONGODB_DBNAME || "operator_app_local";
+
+  const store = await makeMongoDBStore({
+    name: "operator-app",
+    url: mongodbUrl,
+    dbName: mongodbDatabase,
+    models: [LicenseeModel],
+  });
+
+  const journey = createJourney({
+    server: {
+      key: process.env.EVENTS_SERVER_KEY!,
+      secret: process.env.EVENTS_SERVER_SECRET!,
+      url: process.env.EVENTS_SERVER_URL!,
+    },
+    stores: [store],
+    onError(err, event) {
+      console.error(err, event);
+    },
+    onReport(name, msg) {
+      // don't log in test env
+      if (process.env.NODE_ENV === "test") {
+        return;
+      }
+
+      console.log("%s > %s | %j", "JERNI", name, msg.data ?? "");
+    },
+  });
+
+  return journey;
+}
---
+++ packages/regressions/sample1/licensee.ts
@@ -0,0 +1,77 @@
+import { MongoDBModel } from "@jerni/store-mongodb";
+import { MongoOps } from "@jerni/store-mongodb/lib/src/types";
+import mapEvents from "jerni/lib/mapEvents";
+import { omitBy, isNil } from "lodash/fp";
+
+export interface LicenseeDocumentModel {
+  id: string;
+  name: string;
+  slug: string;
+  description: string;
+  createdAt: number;
+}
+
+declare module "jerni" {
+  interface JerniSubscribedEvents {
+    "SCANNER:LICENSEE_CREATED": {
+      id: string;
+      name: string;
+      description: string;
+      created_at: number;
+      created_by: string;
+
+      // new
+      slug: string;
+    };
+
+    "SCANNER:LICENSEE_UPDATED": {
+      id: string;
+      name: string;
+      description?: string;
+      deleted?: boolean;
+    };
+  }
+}
+
+export const LicenseeModel = new MongoDBModel({
+  name: "licensees",
+  version: "1",
+  transform: mapEvents<MongoOps<LicenseeDocumentModel>>({
+    "SCANNER:LICENSEE_CREATED"(event) {
+      const { id, name, description, created_at, slug } = event.payload;
+
+      return {
+        insertOne: {
+          id,
+          name,
+          description,
+          createdAt: created_at,
+          slug,
+        },
+      };
+    },
+    "SCANNER:LICENSEE_UPDATED"(event) {
+      const { id, name, description, deleted } = event.payload;
+
+      if (deleted) {
+        return {
+          deleteOne: {
+            where: { id },
+          },
+        };
+      }
+
+      return {
+        updateOne: {
+          where: { id },
+          changes: {
+            $set: omitBy(isNil, {
+              name,
+              description,
+            }),
+          },
+        },
+      };
+    },
+  }),
+});
