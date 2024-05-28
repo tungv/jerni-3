3945df5aed6eb0f010d648b3d783adcb7fded223
Quan Vo <votrungquan99@gmail.com>
2024-05-28 17:14:03 +0700

    feat: make sure the new events are projected

--- packages/jerni/src/tests/subscribe-from-last-saved-event-id.spec.ts
+++ packages/jerni/src/tests/subscribe-from-last-saved-event-id.spec.ts
@@ -2,9 +2,8 @@ import { expect, test } from "bun:test";
 import { makeMongoDBStore } from "@jerni/store-mongodb";
 import { MongoClient } from "mongodb";
 import { createServer, initJourney, startWorker } from "integration-test-jerni";
-import { BankAccountModel } from "integration-test-jerni/models";
+import { BankAccountModel_2 } from "integration-test-jerni/models";
 import { injectEventDatabase } from "src/events-storage/injectDatabase";
-import { setTimeout } from "node:timers/promises";
 
 import "src/events-storage/__mocks__/sqlite-database";
 
@@ -29,13 +28,13 @@ test(
       name: "mongodb-app",
       url: "mongodb://127.1:27017/",
       dbName,
-      models: [BankAccountModel],
+      models: [BankAccountModel_2],
     });
 
     const worker = await initJourney([storeMongoDbForWorker], port);
 
     // commit event, this should be trigger from app, but for testing purpose, we trigger it from worker
-    const event1 = await worker.journey.commit<"NEW_ACCOUNT_REGISTERED">({
+    const event1 = await worker.journey.commit({
       type: "NEW_ACCOUNT_REGISTERED",
       payload: {
         id: "123",
@@ -51,15 +50,29 @@ test(
 
     ctrl1.abort();
 
+    const event2 = await worker.journey.append({
+      type: "ACCOUNT_DEPOSITED",
+      payload: {
+        id: "123",
+        amount: 100,
+      },
+    });
+
     // STEP 2: start worker again, expect the worker subscribe with last-event-id = 1
     startWorker(worker.journey, ctrl2.signal);
 
-    // wait for the worker to start and subscribe
-    await setTimeout(300);
+    await worker.journey.waitFor(event2);
 
     ctrl2.abort();
 
     const lastCall = inputSpies.subscriptionInputSpy.mock.calls[inputSpies.subscriptionInputSpy.mock.calls.length - 1];
     expect(lastCall[1].headers.get("last-event-id")).toBe("1");
+
+    const BankAccounts = await worker.journey.getReader(BankAccountModel_2);
+    const bankAccount = await BankAccounts.findOne({
+      id: "123",
+    });
+
+    expect(bankAccount.balance).toEqual(100);
   }),
 );
