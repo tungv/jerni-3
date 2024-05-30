e1d363a0a55eb45755d943577768a10cb7332907
Quan Vo <votrungquan99@gmail.com>
2024-05-30 15:26:04 +0700

    fix: use a map to store read pipeline result instead of array
    - the key are the hash of the pipeline instead of the index of the pipeline call

--- packages/jerni-store-mongodb/src/read.ts
+++ packages/jerni-store-mongodb/src/read.ts
@@ -1,17 +1,16 @@
 import type { Db, Document, UpdateFilter } from "mongodb";
 import getCollectionName from "./getCollectionName";
-import type MongoDBModel from "./model";
 import type { JourneyCommittedEvent } from "./types";
+import type MongoDBModel from "./model";
+import hash from "object-hash";
 
 export class Signal<DocumentType extends Document> {
   private model: MongoDBModel<DocumentType>;
   private pipeline: UpdateFilter<DocumentType>[];
-  private slotIndex: number;
 
-  constructor(model: MongoDBModel<DocumentType>, pipeline: UpdateFilter<DocumentType>[], slotIndex: number) {
+  constructor(model: MongoDBModel<DocumentType>, pipeline: UpdateFilter<DocumentType>[]) {
     this.model = model;
     this.pipeline = pipeline;
-    this.slotIndex = slotIndex;
   }
 
   async execute(db: Db) {
@@ -20,16 +19,18 @@ export class Signal<DocumentType extends Document> {
 
     const res = await collection.aggregate(this.pipeline).toArray();
 
+    console.log("executed signal for model=%s, got:", this.model.name, res);
+
     // write to the slot
     let slots = modelSlotsMap.get(this.model);
 
     if (!slots) {
-      slots = [];
+      slots = new Map();
       modelSlotsMap.set(this.model, slots);
     }
 
-    console.log("writing to model=%s slot[%d]", this.model.name, this.slotIndex, res);
-    slots[this.slotIndex] = res;
+    // console.log("writing to model=%s slot[%d]", this.model.name, this.slotIndex, res);
+    slots.set(hash(this.pipeline), res);
 
     return res;
   }
@@ -37,8 +38,7 @@ export class Signal<DocumentType extends Document> {
 
 // const modelSlotsMap = new Map<MongoDBModel<Document>, Array<Document[] | null> | null>();
 // biome-ignore lint/suspicious/noExplicitAny: 'Document' is assignable to the constraint of type 'DocumentType', but 'DocumentType' could be instantiated with a different subtype of constraint 'Document'
-const modelSlotsMap = new Map<MongoDBModel<any>, Array<Document[] | null> | null>();
-let currentModelSlotIndex = 0;
+const modelSlotsMap = new Map<MongoDBModel<any>, Map<any, any[] | null>>();
 
 // let currentModel: MongoDBModel<Document> | null = null;
 // biome-ignore lint/suspicious/noExplicitAny: 'Document' is assignable to the constraint of type 'DocumentType', but 'DocumentType' could be instantiated with a different subtype of constraint 'Document'
@@ -49,7 +49,6 @@ export function runWithModel<DocumentType extends Document>(
   event: JourneyCommittedEvent,
 ) {
   currentModel = model;
-  currentModelSlotIndex = 0;
 
   const res = model.transform(event);
   currentModel = null;
@@ -63,7 +62,7 @@ export function clearModelSlots() {
 
 export default function readPipeline<DocumentType extends Document>(
   pipeline: UpdateFilter<DocumentType>[],
-): Document[] {
+): DocumentType[] {
   const model = currentModel;
 
   if (!model) {
@@ -72,18 +71,17 @@ export default function readPipeline<DocumentType extends Document>(
 
   const slots = modelSlotsMap.get(model);
   if (!slots) {
-    const slot: Array<Document[]> = [];
+    const slot = new Map();
     modelSlotsMap.set(model, slot);
-    throw new Signal<DocumentType>(model, pipeline, 0);
+    throw new Signal<DocumentType>(model, pipeline);
   }
 
-  const res = slots[currentModelSlotIndex];
-  console.log("reading from model=%s slot[%d], got:", model.name, currentModelSlotIndex, res);
+  const res = slots.get(hash(pipeline));
+  console.log("reading from model=%s, got:", model.name, res);
 
   if (res == null) {
-    throw new Signal<DocumentType>(model, pipeline, currentModelSlotIndex);
+    throw new Signal<DocumentType>(model, pipeline);
   }
 
-  currentModelSlotIndex++;
   return res;
 }
