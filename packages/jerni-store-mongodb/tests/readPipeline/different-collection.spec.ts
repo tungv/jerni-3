86fd82e64a526c51784fe42880a16963f57ebcc9
Quan Vo <votrungquan99@gmail.com>
2024-05-31 13:25:32 +0700

    test: add test to make sure the readPipeline clear cache after process the event

--- packages/jerni-store-mongodb/tests/readPipeline/different-collection.spec.ts
+++ packages/jerni-store-mongodb/tests/readPipeline/different-collection.spec.ts
@@ -3,6 +3,7 @@ import type { JourneyCommittedEvent } from "jerni/type";
 import type MongoDBModel from "src/model";
 import readPipeline from "src/read";
 import makeMongoDBStore from "src/store";
+import type { MongoOps } from "src/types";
 
 interface TestCollection {
   id: number;
@@ -78,6 +79,111 @@ describe("Read Pipeline Different Collection", () => {
       {
         id: 2,
         type: "test",
+        payload: {},
+      },
+    ]);
+  });
+
+  test("the model slots should be cleared after processing an event", async () => {
+    expect.assertions(4);
+
+    const model_1: MongoDBModel<TestCollection> = {
+      name: "model_read_1",
+      version: "1",
+      transform(event: JourneyCommittedEvent): MongoOps<TestCollection>[] {
+        if (event.type === "init") {
+          return [
+            {
+              insertMany: [
+                { id: 1, name: "test-model-1--item-1" },
+                { id: 2, name: "test-model-1--item-2" },
+                { id: 3, name: "test-model-1--item-3" },
+              ],
+            },
+          ];
+        }
+
+        // update the row with id = 1 to have name = "test-model-1--item-1-updated"
+        if (event.type === "test1") {
+          return [
+            {
+              updateOne: {
+                where: { id: 1 },
+                changes: { $set: { name: "test-model-1--item-1-updated" } },
+              },
+            },
+          ];
+        }
+
+        return [];
+      },
+    };
+
+    const model_2: MongoDBModel<TestCollection> = {
+      name: "model_read_2",
+      version: "1",
+      transform(event: JourneyCommittedEvent) {
+        if (event.type === "init") {
+          return [];
+        }
+
+        if (event.type === "test1") {
+          const result = readPipeline<TestCollection>(model_1, [
+            {
+              $match: { id: 1 },
+            },
+            {
+              $project: { name: 1, id: 1 },
+            },
+          ]);
+
+          expect(result[0].name).toBe("test-model-1--item-1");
+          expect(result[0].id).toBe(1);
+
+          return [];
+        }
+
+        if (event.type === "test2") {
+          const result = readPipeline<TestCollection>(model_1, [
+            {
+              $match: { id: 1 },
+            },
+            {
+              $project: { name: 1, id: 1 },
+            },
+          ]);
+
+          // if the model slots are not cleared, the result will be "test-model-1--item-1"
+          expect(result[0].name).toBe("test-model-1--item-1-updated");
+          expect(result[0].id).toBe(1);
+
+          return [];
+        }
+      },
+    };
+
+    const store = await makeMongoDBStore({
+      name: "test_read_pipeline",
+      dbName: "mongodb_store_driver_v4_test_read_pipeline",
+      url: "mongodb://127.0.0.1:27017",
+      models: [model_2, model_1],
+    });
+
+    await store.clean();
+    await store.handleEvents([
+      {
+        id: 1,
+        type: "init",
+        payload: {},
+      },
+      {
+        id: 2,
+        type: "test1",
+        payload: {},
+      },
+      {
+        id: 3,
+        type: "test2",
         payload: {},
       },
     ]);
