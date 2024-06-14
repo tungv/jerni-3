3ddb5c92e102933d7fb52ac3e6e67542c7a1de2a
Quan Vo <votrungquan99@gmail.com>
2024-06-14 13:53:12 +0700

    feat: allow to add dependencies models to an other model

--- packages/jerni-store-mongodb/src/model.ts
+++ packages/jerni-store-mongodb/src/model.ts
@@ -6,21 +6,27 @@ export default class MongoDBModel<DocumentType extends Document> {
   version: string;
   transform: TransformFn<DocumentType>;
   meta?: StoreMeta;
+  // biome-ignore lint/suspicious/noExplicitAny: no need to care about the document type of dependencies
+  dependencies?: Array<MongoDBModel<any>>;
 
   constructor({
     name,
     version,
     transform,
     meta,
+    dependencies,
   }: {
     name: string;
     version: string;
     transform: TransformFn<DocumentType>;
     meta?: StoreMeta;
+    // biome-ignore lint/suspicious/noExplicitAny: no need to care about the document type of dependencies
+    dependencies?: Array<MongoDBModel<any>>;
   }) {
     this.name = name;
     this.version = version;
     this.transform = transform;
     this.meta = meta;
+    this.dependencies = dependencies;
   }
 }
