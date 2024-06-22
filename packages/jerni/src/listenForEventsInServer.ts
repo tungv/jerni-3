7e2e4af7b4fd915da25915af67f7b77e40c95723
Quan Vo <votrungquan99@gmail.com>
2024-06-22 13:39:58 +0700

    refactor: extract the start listening to a function to reuse easier

--- packages/jerni/src/listenForEventsInServer.ts
+++ packages/jerni/src/listenForEventsInServer.ts
@@ -1,5 +1,23 @@
+import { EventSourcePlus } from "event-source-plus";
+import { URL } from "node:url";
+import type { Logger } from "./types/Logger";
+
 const HIGH_WATER_MARK = 1000000; // 1,000,000
 const LOW_WATER_MARK = 100000; // 100,000
+
+function startListening(
+  subscriptionUrl: URL,
+  eventsBuffer: string[],
+  resolvePromise: (value?: unknown) => void,
+  stop: (stopAtEventId: string) => void,
+  logger: Logger,
+) {
+  const eventSource = new EventSourcePlus(subscriptionUrl.toString(), {
+    headers: {
+      authorization: `Basic ${btoa(`${subscriptionUrl.username}:${subscriptionUrl.password}`)}`,
+      "burst-count": "200",
+    },
+  });
 
   const controller = eventSource.listen({
     onMessage(message) {
@@ -40,6 +58,34 @@ const LOW_WATER_MARK = 100000; // 100,000
       console.log({ responseError });
     },
   });
+
+  return controller;
+}
+
+export default async function* listenForEventsInServer(
+  subscriptionUrl: URL,
+  signal: AbortSignal,
+  logger: Logger,
+): AsyncGenerator<string> {
+  const { promise: initialPromise, resolve: initialResolve } = Promise.withResolvers();
+
+  let unResolvedPromise = initialPromise;
+  let resolver = initialResolve;
+  let hasStopped = false;
+
+  function stop(stopAtEventId: string) {
+    // should continue from the next event after stopping
+    const newLastEventId = String(Number(stopAtEventId) + 1);
+    subscriptionUrl.searchParams.set("lastEventId", newLastEventId);
+    hasStopped = true;
+  }
+  function resolvePromise() {
+    resolver();
+  }
+
+  const eventsBuffer: string[] = [];
+
+  let controller = startListening(subscriptionUrl, eventsBuffer, resolvePromise, stop, logger);
 
   signal.addEventListener(
     "abort",
