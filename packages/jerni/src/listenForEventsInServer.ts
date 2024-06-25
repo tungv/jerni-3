a2b8dfa95565e7a37563458b89880c1431babd08
Quan Vo <votrungquan99@gmail.com>
2024-06-25 14:40:54 +0700

    fix: remove adding last-event-id to headers since it was added in the search params

--- packages/jerni/src/listenForEventsInServer.ts
+++ packages/jerni/src/listenForEventsInServer.ts
@@ -1,5 +1,5 @@
+import type { URL } from "node:url";
 import { EventSourcePlus } from "event-source-plus";
-import { URL } from "node:url";
 import type { Logger } from "./types/Logger";
 
 const HIGH_WATER_MARK = 1000000; // 1,000,000
@@ -33,26 +33,6 @@ function startListening(
           stop(message.id ?? "0");
         }
       }
-    },
-    onRequest(context) {
-      // check if the last event id in the header or query is higher, then we should set the last event id in header to that
-      const stringHeaderLastEventId = context.options.headers
-        ? (context.options.headers as Record<string, string>)["Last-Event-ID"]
-        : null;
-      const headerLastEventId = stringHeaderLastEventId ? Number.parseInt(stringHeaderLastEventId) : 0;
-      const url = new URL(context.request);
-      const stringQueryLastEventId = url.searchParams.get("lastEventId");
-      const queryLastEventId = stringQueryLastEventId ? Number.parseInt(stringQueryLastEventId) : 0;
-
-      const lastEventId = Math.max(headerLastEventId, queryLastEventId);
-      context.options.headers = {
-        ...context.options.headers,
-        "Last-Event-ID": lastEventId.toString(),
-      };
-
-      // also remove the last event id from the search params
-      url.searchParams.delete("lastEventId");
-      context.request = url.toString();
     },
     onResponseError(responseError) {
       console.log({ responseError });
