25107436c74e458dfff57b45c7886e9766128ed1
Tung Vu <me@tungv.com>
2024-06-26 10:22:04 +0700

    refactor: narrow down the requirement for `model` argument
    Signed-off-by: Tung Vu <me@tungv.com>

--- packages/jerni-store-mongodb/src/getCollectionName.ts
+++ packages/jerni-store-mongodb/src/getCollectionName.ts
@@ -1,6 +1,3 @@
-import type MongoDBModel from "./model";
-
-// biome-ignore lint/suspicious/noExplicitAny: any is used to prevent Typescript constraint error when calling getCollectionName
-export default function getCollectionName(model: MongoDBModel<any>) {
+export default function getCollectionName(model: { name: string; version: string }) {
   return `${model.name}_v${model.version}`;
 }
