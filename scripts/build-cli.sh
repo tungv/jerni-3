55b0b1594f852f0152c2b363eb6145168f44170f
Quan Vo <votrungquan99@gmail.com>
2024-07-03 11:27:32 +0700

    chore: setup script to build cli

---
+++ scripts/build-cli.sh
@@ -0,0 +1,14 @@
+
+# isolate the jerni package to temporary ignore the other packages
+mkdir build
+mkdir build/packages
+mkdir build/packages/jerni
+cp package.json build/
+cp bun.lockb build/
+cp -r packages/jerni build/packages/jerni/
+
+# install the dependencies
+cd build
+bun install
+cd packages/jerni
+bun build ./src/cli.ts --compile --outfile ./src/mycli
