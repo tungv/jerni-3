4151f8518b7adce6722d24aa89defdd0553000d1
Quan Vo <votrungquan99@gmail.com>
2024-07-04 19:14:31 +0700

    chore: do not ignore other packages when release jsr

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,11 +1,11 @@
-# isolate the jerni package to temporary ignore the other packages
-mkdir build
-mkdir build/packages
-mkdir build/packages/jerni
-cp package.json build/
-cp bun.lockb build/
-cp -r packages/jerni build/packages/
+# Link all the packages that integrations uses. there is no way we can install only for jerni package
+# when just copy the jerni package, bun does not create a node_modules folder in the jerni package, only in the root folder
+# hence, jsr can not recognized the packages uses in jerni
+cd packages/jerni
+bun link
+cd ../../packages/jerni-store-mongodb
+bun link
+cd ../..
 
-# install the dependencies
-cd build/packages/jerni
+# install all the dependencies, node_modules folders will be created in all the packages
 bun install
