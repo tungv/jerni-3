4ced8351b1d1c2c6fcffeb67bde1fde7782d1aba
Quan Vo <votrungquan99@gmail.com>
2024-07-04 19:26:10 +0700

    chore: fix publish jsr step

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,11 +1,6 @@
-# Link all the packages that integrations uses. there is no way we can install only for jerni package
-# when just copy the jerni package, bun does not create a node_modules folder in the jerni package, only in the root folder
-# hence, jsr can not recognized the packages uses in jerni
-cd packages/jerni
-bun link
-cd ../../packages/jerni-store-mongodb
-bun link
-cd ../..
+# isolate jerni so that all the dependencies are installed in the build directory
+cp -r packages/jerni build
 
-# install all the dependencies, node_modules folders will be created in all the packages
+# install all the dependencies, node_modules with all packages will be installed
+cd build
 bun install
