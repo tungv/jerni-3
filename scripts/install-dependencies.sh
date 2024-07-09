c7b9c1f673e888518354151b0ec8fb9e1f4aa8cb
Quan Vo <votrungquan99@gmail.com>
2024-07-09 16:45:28 +0700

    gh-actions(publish-jsr): remove integration package to not worrying about linking

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,6 +1,12 @@
-# isolate jerni so that all the dependencies are installed in the build directory
-cp -r packages/jerni build
+# # isolate jerni so that all the dependencies are installed in the build directory
+# cp -r packages/jerni build
+
+# # install all the dependencies, node_modules with all packages will be installed
+# cd build
+# bun install
+
+# remove the integrations dir so that we don't have to worry about linking packages yet
+rm -rf packages/integrations
 
 # install all the dependencies, node_modules with all packages will be installed
-cd build
 bun install
