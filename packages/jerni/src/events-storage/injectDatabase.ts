21bc75154b0863f8310caf9a4720bf78965b50b3
Quan Vo <votrungquan99@gmail.com>
2024-07-15 12:33:47 +0700

    chore: add to do comment

--- packages/jerni/src/events-storage/injectDatabase.ts
+++ packages/jerni/src/events-storage/injectDatabase.ts
@@ -15,6 +15,7 @@ export interface EventDatabase {
 }
 
 async function getDB() {
+  // TODO: improve log here, current settings make it log every time the query is called
   if (process.env.EVENTS_DB_MONGODB_URL && process.env.EVENTS_DB_MONGODB_NAME) {
     console.log("Using MongoDB as event storage");
 
