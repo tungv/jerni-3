6d3b279eaa17208d0585b947c493f3b834e08037
Quan Vo <votrungquan99@gmail.com>
2024-07-24 17:18:58 +0700

    test: should clean start jerni dev when r\n is entered

--- packages/integrations/tests/CLI/jerni-dev.spec.ts
+++ packages/integrations/tests/CLI/jerni-dev.spec.ts
@@ -3,6 +3,7 @@ import { exec } from "node:child_process";
 import fs from "node:fs";
 import path from "node:path";
 import { setTimeout } from "node:timers/promises";
+import hash_sum from "hash-sum";
 import { nanoid } from "nanoid";
 import yaml from "yaml";
 import cleanUpTestDatabase from "../cleanUpTestDatabase";
@@ -152,6 +153,129 @@ describe("Jerni Dev Integration", () => {
     });
 
     expect(bankAccount2).toEqual({
+      __op: 0,
+      __v: 1,
+      _id: expect.anything(),
+      id: "123",
+      name: "test",
+      balance: 0,
+    });
+
+    childProcess.kill();
+  });
+
+  test("should clean start jerni dev when r\n is entered", async () => {
+    // random a port
+    const port = Math.floor(Math.random() * 10000) + 10000;
+    const dbFileName = `./test-events-db-${nanoid()}.yml`;
+    const mongodbName = `jerni_integration_test_${nanoid()}`;
+
+    const devCliPath = path.resolve(__dirname, "../../../jerni/src/dev-cli/index.ts");
+    const initFileName = path.resolve(__dirname, "./makeTestJourneyCli.ts");
+    const dbFilePath = path.resolve(__dirname, dbFileName);
+
+    const childProcess = exec(
+      `PORT=${port}\
+        MONGODB_DBNAME=${mongodbName}\
+        bun run ${devCliPath} ${initFileName} ${dbFilePath}`,
+      (error, stdout, stderr) => {
+        // console.log(`stdout: ${stdout}`);
+        // console.error(`stderr: ${stderr}`);
+      },
+    );
+
+    await setTimeout(1000);
+
+    // write r and press enter to child process
+    if (!childProcess.stdin) {
+      throw new Error("child process has no stdin");
+    }
+    childProcess.stdin.write("r\n");
+
+    process.env.EVENTS_SERVER = `http://localhost:${port}`;
+    const mongoConfig = {
+      url: "mongodb://localhost:27017",
+      dbName: mongodbName,
+    };
+    const journey = await initializeJourney(mongoConfig);
+
+    // commit 2 events
+    const event1 = await journey.append<"NEW_ACCOUNT_REGISTERED">({
+      type: "NEW_ACCOUNT_REGISTERED",
+      payload: {
+        id: "123",
+        name: "test",
+      },
+    });
+    const event2 = await journey.append<"ACCOUNT_DEPOSITED">({
+      type: "ACCOUNT_DEPOSITED",
+      payload: {
+        id: "123",
+        amount: 100,
+      },
+    });
+
+    await journey.waitFor(event2);
+    // await setTimeout(1000);
+
+    // check balance to be 100
+    const BankAccounts = await journey.getReader(BankAccountModel_2);
+
+    const bankAccount1 = await BankAccounts.findOne({
+      id: "123",
+    });
+
+    expect(bankAccount1).toEqual({
+      __op: 0,
+      __v: 2,
+      _id: expect.anything(),
+      id: "123",
+      name: "test",
+      balance: 100,
+    });
+
+    // remove the last event and rewrite to the db file, so that the checksum will match, no new events, so the data should not be clean
+    const fileContent = fs.readFileSync(dbFilePath, "utf8");
+
+    const parsedContent = yaml.parse(fileContent);
+    parsedContent.events.pop();
+    const newChecksum = hash_sum(parsedContent.events);
+    parsedContent.checksum = newChecksum;
+    const stringifiedContent = yaml.stringify(parsedContent);
+
+    fs.writeFileSync(dbFilePath, stringifiedContent);
+
+    // wait for the file to be read, and restart jerni dev
+    await setTimeout(1000);
+
+    // the data in the db should have balance 0
+    const bankAccount2 = await BankAccounts.findOne({
+      id: "123",
+    });
+
+    expect(bankAccount2).toEqual({
+      __op: 0,
+      __v: 2,
+      _id: expect.anything(),
+      id: "123",
+      name: "test",
+      balance: 100,
+    });
+
+    // write r and press enter to child process
+    if (!childProcess.stdin) {
+      throw new Error("child process has no stdin");
+    }
+    childProcess.stdin.write("r\n");
+
+    await setTimeout(1000);
+
+    // the data in the db should have balance 0
+    const bankAccount3 = await BankAccounts.findOne({
+      id: "123",
+    });
+
+    expect(bankAccount3).toEqual({
       __op: 0,
       __v: 1,
       _id: expect.anything(),
