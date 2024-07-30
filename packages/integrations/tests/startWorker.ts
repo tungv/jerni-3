0af9e38db987045f6e949c320e5d378ee889f21b
Quan Vo <votrungquan99@gmail.com>
2024-07-30 22:56:15 +0700

    tests: use mongodb as event db for integration test

--- packages/integrations/tests/startWorker.ts
+++ packages/integrations/tests/startWorker.ts
@@ -1,7 +1,13 @@
+import begin from "@jerni/jerni-3/lib/begin";
 import type { JourneyInstance } from "@jerni/jerni-3/types";
-import begin from "jerni/lib/begin";
+import { nanoid } from "nanoid";
 
 export default async function startWorker(journey: JourneyInstance, signal: AbortSignal) {
+  const eventDbName = `jerni_integration_test_events_${nanoid()}`;
+
+  process.env.EVENTS_DB_MONGODB_URL = "mongodb://127.0.0.1:27017";
+  process.env.EVENTS_DB_MONGODB_NAME = eventDbName;
+
   for await (const events of begin(journey, signal)) {
     // console.log("events", events);
   }
