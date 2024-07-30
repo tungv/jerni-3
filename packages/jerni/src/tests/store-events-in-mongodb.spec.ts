a01726030e6a23bf8a3996c41c48fe31347cade9
Quan Vo <votrungquan99@gmail.com>
2024-07-30 22:13:21 +0700

    test: directly inject env to check

--- packages/jerni/src/tests/store-events-in-mongodb.spec.ts
+++ packages/jerni/src/tests/store-events-in-mongodb.spec.ts
@@ -4,48 +4,52 @@ import begin from "../begin";
 import createServer from "./helpers/events-server";
 import { initJourney } from "./helpers/initJourney";
 
-import injectTestEventsMongoDB from "src/events-storage/__mocks__/mongodb-database";
-
 describe("Persist event to file system", () => {
-  test(
-    "received events should be persist to mongodb database",
-    injectTestEventsMongoDB(async () => {
-      const { server } = createServer([
-        {
-          id: 1,
-          type: "NEW_ACCOUNT_REGISTERED",
-          payload: {
-            id: "123",
-            name: "test",
-          },
+  test("received events should be persist to mongodb database", async () => {
+    process.env.EVENTS_DB_MONGODB_URL = "mongodb://localhost:27017";
+    process.env.EVENTS_DB_MONGODB_NAME = "jerni_events_test";
+
+    const { server } = createServer([
+      {
+        id: 1,
+        type: "NEW_ACCOUNT_REGISTERED",
+        payload: {
+          id: "123",
+          name: "test",
         },
-      ]);
-      const eventServerUrl = `http://localhost:${server.port}`;
+      },
+    ]);
+    const eventServerUrl = `http://localhost:${server.port}`;
 
-      const ctrl = new AbortController();
+    const ctrl = new AbortController();
 
-      const worker = await initJourney(eventServerUrl, []);
+    const worker = await initJourney(eventServerUrl, []);
 
-      // stop after the first event is processed and yielded
-      for await (const _events of begin(worker.journey, ctrl.signal)) {
-        ctrl.abort();
-      }
+    // stop after the first event is processed and yielded
+    for await (const _events of begin(worker.journey, ctrl.signal)) {
+      ctrl.abort();
+    }
 
-      const client = await MongoClient.connect("mongodb://127.0.0.1:27017");
-      const db = client.db(process.env.EVENTS_DB_MONGODB_NAME);
-      const persistedEvents = await db.collection("events").find().toArray();
+    const client = await MongoClient.connect("mongodb://127.0.0.1:27017");
+    const db = client.db("jerni_events_test");
+    const persistedEvents = await db.collection("events").find().toArray();
 
-      expect(persistedEvents).toEqual([
-        {
-          _id: expect.anything(),
-          id: 1,
-          type: "NEW_ACCOUNT_REGISTERED",
-          payload: {
-            id: "123",
-            name: "test",
-          },
+    expect(persistedEvents).toEqual([
+      {
+        _id: expect.anything(),
+        id: 1,
+        type: "NEW_ACCOUNT_REGISTERED",
+        payload: {
+          id: "123",
+          name: "test",
         },
-      ]);
-    }),
-  );
+      },
+    ]);
+
+    await client.db("jerni_events_test").dropDatabase();
+
+    await client.close();
+    process.env.EVENTS_DB_MONGODB_NAME = undefined;
+    process.env.EVENTS_DB_MONGODB_URL = undefined;
+  });
 });
