1b726ba6f3dfffb24bc36454f75cef737f41dd5d
Quan Vo <votrungquan99@gmail.com>
2024-07-31 11:32:30 +0700

    fix(test-wrapper): test instance can commit any local event

--- packages/jerni/src/types/JourneyTestInstance.ts
+++ packages/jerni/src/types/JourneyTestInstance.ts
@@ -1,7 +1,14 @@
-import type { JourneyCommittedEvent } from "./events";
+import type {
+  JourneyCommittedEvent,
+  LocalEvents,
+  ToBeCommittedJourneyEvent,
+  TypedJourneyCommittedEvent,
+} from "./events";
 import type { JourneyInstance } from "./journey";
 
-export interface JourneyTestInstance extends JourneyInstance {
+export interface JourneyTestInstance extends Omit<JourneyInstance, "commit" | "append"> {
+  commit<P extends keyof LocalEvents>(event: ToBeCommittedJourneyEvent<P>): Promise<TypedJourneyCommittedEvent<P>>;
+  append<P extends keyof LocalEvents>(event: ToBeCommittedJourneyEvent<P>): Promise<TypedJourneyCommittedEvent<P>>;
   waitAll(): Promise<void>;
   committed: JourneyCommittedEvent[];
 }
