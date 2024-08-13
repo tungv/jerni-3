3d88bfb2997a148dbb507ef31287da49958c4542
Tung Vu <me@tungv.com>
2024-08-13 14:59:56 +0700

    feat: implement new event source adapter
    Signed-off-by: Tung Vu <me@tungv.com>

---
+++ packages/jerni/src/getEventStream.ts
@@ -0,0 +1,159 @@
+import type { URL } from "node:url";
+import { type EventSourceController, EventSourcePlus } from "event-source-plus";
+import messageListFromString from "./getMessage";
+import type { Logger } from "./types/Logger";
+import type { JourneyCommittedEvent } from "./types/events";
+
+const THIRTY_SECONDS = 30 * 1000;
+
+const RETRY_TIMES = [10, 20, 30, 60, 120, 300, 600, 1200, 1800, 3600];
+
+export default async function getEventStreamFromUrl(
+  initialFrom: string,
+  url: URL,
+  signal: AbortSignal,
+  logger: Logger,
+) {
+  console.log("getEventStreamFromUrl", url.toString());
+  let currentFrom = initialFrom;
+  let retryTime = 10;
+  let errorCount = 0;
+  let idleTime = THIRTY_SECONDS;
+
+  return new ReadableStream<JourneyCommittedEvent[]>({
+    async start(controller) {
+      while (!signal.aborted) {
+        const connectionStart = Date.now();
+        try {
+          const eventStream = retrieveJourneyCommittedEvents(url, currentFrom, idleTime, signal);
+
+          for await (const msg of eventStream) {
+            if (msg.type === "connected") {
+              errorCount = 0;
+              const connectionTime = msg.connected_at - connectionStart;
+              logger.log(`connected after ${connectionTime}ms`);
+              continue;
+            }
+
+            if (msg.type === "idle") {
+              logger.log(`idle for ${msg.idle_period}ms`);
+              // double the idle time
+              idleTime *= 2;
+              break;
+            }
+
+            const eventBatch = msg.events;
+
+            const lastEvent = eventBatch.at(-1);
+            if (lastEvent) {
+              // reset idle time
+              idleTime = THIRTY_SECONDS;
+              currentFrom = String(lastEvent.id);
+              controller.enqueue(eventBatch);
+            }
+          }
+
+          // has returned due to counterBeforeReset
+          retryTime = 10;
+          logger.log("reconnect due to inactivity");
+        } catch (ex) {
+          errorCount++;
+          retryTime = RETRY_TIMES[errorCount] ?? RETRY_TIMES.at(-1);
+          logger.error("reconnect due to error", ex);
+        }
+
+        Bun.gc(false);
+        await Bun.sleep(retryTime);
+      }
+    },
+  });
+}
+
+type EventStreamReturnType =
+  | {
+      type: "connected";
+      connected_at: number;
+    }
+  | {
+      type: "idle";
+      idle_period: number;
+    }
+  | {
+      type: "event";
+      events: JourneyCommittedEvent[];
+    };
+
+async function* retrieveJourneyCommittedEvents(
+  url: URL,
+  lastSeenId: string,
+  idleTime: number,
+  signal: AbortSignal,
+): AsyncGenerator<EventStreamReturnType, void, unknown> {
+  try {
+    const resp = await fetch(url.toString(), {
+      headers: {
+        authorization: `Basic ${btoa(`${url.username}:${url.password}`)}`,
+        "burst-count": "200",
+        "last-event-id": lastSeenId,
+      },
+      signal,
+    });
+
+    if (!resp.ok) {
+      throw new Error("failed to fetch");
+    }
+
+    if (!resp.body) {
+      throw new Error("response body is empty");
+    }
+
+    yield {
+      type: "connected" as const,
+      connected_at: Date.now(),
+    };
+
+    const stream = resp.body;
+
+    let pending = "";
+
+    const decoder = new TextDecoder();
+
+    let timeSinceLastData = Date.now();
+
+    // read the body
+    for await (const chunk of stream) {
+      const utf8 = decoder.decode(chunk, { stream: true });
+      pending += utf8;
+
+      const r = messageListFromString(pending);
+
+      pending = r.leftoverData;
+
+      const events = r.messages.flatMap((message) => {
+        if (message.event === "INCMSG") {
+          return JSON.parse(message.data) as JourneyCommittedEvent[];
+        }
+
+        return [];
+      });
+
+      if (events.length > 0) {
+        yield {
+          type: "event",
+          events,
+        };
+        timeSinceLastData = Date.now();
+      }
+
+      const elapsed = Date.now() - timeSinceLastData;
+      if (elapsed > idleTime) {
+        yield {
+          type: "idle",
+          idle_period: elapsed,
+        };
+      }
+    }
+  } finally {
+    Bun.gc(true);
+  }
+}
---
+++ packages/jerni/src/getMessage.ts
@@ -0,0 +1,94 @@
+interface Message {
+  id: string;
+  data: string;
+  event: string;
+  retry?: number;
+}
+
+export default function messageListFromString(input: string) {
+  const messages: Message[] = [];
+  let line = "";
+  let ignoreNextNewline = false;
+  let data: string;
+  let id: string;
+  let event: string;
+  let retry: undefined | number;
+  let previousChar = "";
+  let pendingIndex = 0;
+  let isEndOfMessage = false;
+  function handleParseLine(pIndex: number) {
+    const result = parseLine(line);
+    data = result.data ?? data;
+    id = result.id ?? id;
+    event = result.event ?? event;
+    retry = result.retry ?? retry;
+    if (isEndOfMessage) {
+      if (typeof data === "string") {
+        messages.push({
+          id,
+          data,
+          event: event ?? "message",
+          retry,
+        });
+      }
+      id = "";
+      data = "";
+      event = "";
+      retry = void 0;
+      pendingIndex = pIndex;
+    }
+    line = "";
+  }
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+    switch (char) {
+      case "\r": {
+        isEndOfMessage = previousChar === "\n" || previousChar === "\r";
+        ignoreNextNewline = true;
+        const pIndex = input[i + 1] === "\n" ? i + 2 : i + 1;
+        handleParseLine(pIndex);
+        break;
+      }
+      case "\n": {
+        if (ignoreNextNewline) {
+          ignoreNextNewline = false;
+          break;
+        }
+        isEndOfMessage = previousChar === "\n";
+        handleParseLine(i + 1);
+        break;
+      }
+      default:
+        line += char;
+        break;
+    }
+    previousChar = char;
+  }
+  return {
+    messages,
+    leftoverData: input.substring(pendingIndex),
+  };
+}
+function parseLine(input: string) {
+  if (input.startsWith("data:")) {
+    return { data: input.substring(5).trim() };
+  }
+  if (input.startsWith("id:")) {
+    return { id: input.substring(3).trim() };
+  }
+  if (input.startsWith("event:")) {
+    return {
+      event: input.substring(6).trim(),
+    };
+  }
+  if (input.startsWith("retry:")) {
+    const val = Number(input.substring(6).trim());
+    if (!Number.isNaN(val)) {
+      if (Number.isInteger(val)) {
+        return { retry: val };
+      }
+      return { retry: Math.round(val) };
+    }
+  }
+  return {};
+}
