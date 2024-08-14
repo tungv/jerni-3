95f72e97619b5ec14ea4ce0a1303e0a2070e99be
Tung Vu <me@tungv.com>
2024-08-14 14:16:30 +0700

    chore: make typing more generic

--- packages/jerni-store-mongodb/src/types.ts
+++ packages/jerni-store-mongodb/src/types.ts
@@ -63,7 +63,7 @@ export interface MongoDBStore {
     >,
   ) => void;
 
-  getDriver<T extends Document>(model: MongoDBModel<T>): Promise<Collection<T> & AsyncDisposable>;
+  getDriver<T extends Document>(model: MongoDBModel<T>): Promise<AsyncDisposable>;
   handleEvents: (events: JourneyCommittedEvent[]) => Promise<{ [modelIdentifier: string]: Changes }>;
   getLastSeenId: () => Promise<number>;
   toString(): string;
