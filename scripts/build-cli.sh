55600665ad266fd7d29627c2212b18bc9948642d
Tung Vu <me@tungv.com>
2024-08-19 16:39:46 +0700

    chore: force smol for jerni cli
    Signed-off-by: Tung Vu <me@tungv.com>

--- scripts/build-cli.sh
+++ scripts/build-cli.sh
@@ -5,11 +5,11 @@ cp -r packages/jerni build
 cd build
 bun install
 
-bun build ./src/cli.ts --compile --target=bun-linux-x64 --outfile ./src/jerni-linux-x64
-bun build ./src/cli.ts --compile --target=bun-darwin-x64 --outfile ./src/jerni-darwin-x64
-bun build ./src/cli.ts --compile --target=bun-windows-x64 --outfile ./src/jerni-win-x64
-bun build ./src/cli.ts --compile --target=bun-linux-arm64 --outfile ./src/jerni-linux-arm64
-bun build ./src/cli.ts --compile --target=bun-darwin-arm64 --outfile ./src/jerni-darwin-arm64
+bun build ./src/cli.ts --compile --smol --target=bun-linux-x64 --outfile ./src/jerni-linux-x64
+bun build ./src/cli.ts --compile --smol --target=bun-darwin-x64 --outfile ./src/jerni-darwin-x64
+bun build ./src/cli.ts --compile --smol --target=bun-windows-x64 --outfile ./src/jerni-win-x64
+bun build ./src/cli.ts --compile --smol --target=bun-linux-arm64 --outfile ./src/jerni-linux-arm64
+bun build ./src/cli.ts --compile --smol --target=bun-darwin-arm64 --outfile ./src/jerni-darwin-arm64
 
 # build cli for dev
 bun build ./src/dev-cli/index.ts --compile --target=bun-linux-x64 --outfile ./src/jerni-dev-linux-x64
