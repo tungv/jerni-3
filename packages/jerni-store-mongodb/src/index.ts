79054c9c55ddf19ebb5b3caf35542f55746180a6
Tung Vu <me@tungv.com>
2024-08-21 14:23:57 +0700

    feat: add `large()` api to mark an operation is large
    Signed-off-by: Tung Vu <me@tungv.com>

--- packages/jerni-store-mongodb/src/index.ts
+++ packages/jerni-store-mongodb/src/index.ts
@@ -1,6 +1,7 @@
 import getCollectionName from "./getCollectionName";
 import MongoDBModel from "./model";
 import readPipeline from "./read";
+import { large } from "./read";
 import makeMongoDBStore from "./store";
 
-export { readPipeline, MongoDBModel, makeMongoDBStore, getCollectionName };
+export { readPipeline, MongoDBModel, makeMongoDBStore, getCollectionName, large };
