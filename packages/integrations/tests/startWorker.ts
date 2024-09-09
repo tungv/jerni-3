6307feefe7bf19dcae8c7469c5edcd963cd6caaf
Quan Vo <votrungquan99@gmail.com>
2024-09-09 17:00:30 +0700

    chore: remove footprint of event mongodb

--- packages/integrations/tests/startWorker.ts
+++ packages/integrations/tests/startWorker.ts
@@ -1,14 +1,10 @@
 import begin from "@jerni/jerni-3/lib/begin";
 import type { JourneyInstance } from "@jerni/jerni-3/types";
-import { nanoid } from "nanoid";
 
 export default async function startWorker(journey: JourneyInstance, signal: AbortSignal) {
-  const eventDbName = `jerni_integration_test_events_${nanoid()}`;
-
-  process.env.EVENTS_DB_MONGODB_URL = "mongodb://127.0.0.1:27017";
-  process.env.EVENTS_DB_MONGODB_NAME = eventDbName;
-
   for await (const events of begin(journey, signal)) {
     // console.log("events", events);
+
+    return events;
   }
 }
