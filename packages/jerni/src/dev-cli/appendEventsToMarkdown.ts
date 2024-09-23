93643f49a53029d410b0849b4425ca0543eb0399
Quan Vo <votrungquan99@gmail.com>
2024-09-23 21:25:57 +0700

    feat: append new events to md file in a details tag to make it look prettier

--- packages/jerni/src/dev-cli/appendEventsToMarkdown.ts
+++ packages/jerni/src/dev-cli/appendEventsToMarkdown.ts
@@ -1,6 +1,6 @@
 import fs from "node:fs/promises";
 import hash_sum from "hash-sum";
-import type { Code, Yaml } from "mdast";
+import type { Root } from "mdast";
 import { fromMarkdown } from "mdast-util-from-markdown";
 import { frontmatterFromMarkdown, frontmatterToMarkdown } from "mdast-util-frontmatter";
 import { toMarkdown } from "mdast-util-to-markdown";
@@ -20,14 +20,7 @@ export default async function appendEventsToMarkdown(filePath: string, events: T
 
   // append the events to the markdown, event node are jsonc with meta ?type=events
   for (const event of events) {
-    const eventNode = {
-      type: "code",
-      lang: "jsonc",
-      value: JSON.stringify(event),
-      meta: "?type=events",
-    } as Code;
-
-    ast.children.push(eventNode);
+    addEventToAst(ast, event);
   }
 
   const allEvents = getEventsFromAst(ast.children);
@@ -48,4 +41,59 @@ export default async function appendEventsToMarkdown(filePath: string, events: T
   });
 
   await fs.writeFile(filePath, newMarkdown);
+}
+
+/**
+ * Adds a journey event to the given markdown AST (Abstract Syntax Tree).
+ * The event is added within a <details> HTML tag, with the event type as the summary.
+ * The event details are added as a JSONC code block with a meta tag ?type=events.
+ *
+ * @example
+ * <details><summary>NEW_ACCOUNT_REGISTERED</summary>
+ *
+ * ```jsonc ?type=events
+ * {
+ *   "type": "NEW_ACCOUNT_REGISTERED",
+ *   "payload": {
+ *     "id": "123",
+ *     "name": "test"
+ *   },
+ *   "meta": {}
+ * }
+ * ```
+ *
+ * </details>
+ */
+function addEventToAst(ast: Root, event: ToBeCommittedJourneyEvent) {
+  // put the events in a details tag
+  ast.children.push({
+    type: "html",
+
+    // Add a newline after the opening tag and summary for proper rendering
+    // Reference: https://gist.github.com/scmx/eca72d44afee0113ceb0349dd54a84a2?permalink_comment_id=4095967
+    value: `\n<details><summary>${event.type}</summary>\n`,
+  });
+
+  ast.children.push(
+    {
+      type: "text",
+      value: "\n",
+    },
+    {
+      type: "code",
+      lang: "jsonc",
+      meta: "?type=events",
+      value: `${JSON.stringify(event, null, 2)}`,
+    },
+  );
+
+  ast.children.push({
+    type: "html",
+    value: "\n\n</details>",
+  });
+
+  ast.children.push({
+    type: "text",
+    value: "\n\n",
+  });
 }
