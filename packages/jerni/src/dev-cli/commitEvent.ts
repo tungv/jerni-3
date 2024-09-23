d4802d605b041113f5a0808eb48f993417aafcb3
Quan Vo <votrungquan99@gmail.com>
2024-09-23 21:21:43 +0700

    feat: debounce appending events to md file to avoid conflict

--- packages/jerni/src/dev-cli/commitEvent.ts
+++ packages/jerni/src/dev-cli/commitEvent.ts
@@ -1,12 +1,30 @@
 import sqlite from "bun:sqlite";
 import type { ToBeCommittedJourneyEvent } from "../types/events";
 import appendEventsToMarkdown from "./appendEventsToMarkdown";
+import debounce from "./debounce";
+
+const debouncedFnByTextFilePath = new Map<string, (arg: ToBeCommittedJourneyEvent[]) => void>();
+
+// curry the appendEventsToMarkdown function with the textFilePath
+// append events to text file in the background, also debounce to prevent conflicts when multiple events are committed at the same time
+function getDebouncedAppendEvents(textFilePath: string) {
+  let debouncedFn = debouncedFnByTextFilePath.get(textFilePath);
+
+  if (!debouncedFn) {
+    const curried = appendEventsToMarkdown.bind(null, textFilePath);
+    debouncedFn = debounce(curried, 300);
+    debouncedFnByTextFilePath.set(textFilePath, debouncedFn);
+  }
+
+  return debouncedFn;
+}
 
 export default function commitEvent(sqliteFilePath: string, textFilePath: string, events: ToBeCommittedJourneyEvent[]) {
   const lastId = writeEventToSqlite(sqliteFilePath, events);
 
-  // append events to text file in the background
-  appendEventsToMarkdown(textFilePath, events);
+  const debouncedAppendEventsToMarkdown = getDebouncedAppendEvents(textFilePath);
+
+  debouncedAppendEventsToMarkdown(events);
 
   return lastId;
 }
