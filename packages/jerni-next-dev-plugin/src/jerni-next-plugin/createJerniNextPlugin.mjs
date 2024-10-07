c12b6b2444c88fd88b82561cc3c6b7bdbce2187b
ttlong3103 <tranthanhlong3103@gmail.com>
2024-10-07 12:10:19 +0700

    implement jerni-next-plugin

---
+++ packages/jerni-next-dev-plugin/src/jerni-next-plugin/createJerniNextPlugin.mjs
@@ -0,0 +1,44 @@
+import path from "node:path";
+import scheduleCleanStartJerni from "./cleanStartJerniDev.mjs";
+import getFilesToWatch from "./getFilesToWatch.mjs";
+
+export default async function createJerniNextPlugin(config) {
+  const { initializerAbsoluteFilePath, eventsFileAbsolutePath, sqliteFileAbsolutePath } = config;
+  console.log(JSON.stringify(config, null, 2));
+
+  /**
+   * Get modules imported in the initializer file
+   * @type {string[]}
+   */
+  let jerniDeps = await getFilesToWatch(initializerAbsoluteFilePath).then((files) =>
+    files.concat(eventsFileAbsolutePath),
+  );
+
+  // sync binary and clean start
+  scheduleCleanStartJerni(eventsFileAbsolutePath, sqliteFileAbsolutePath);
+
+  return function jerniNextPlugin() {
+    // watchRun hook executes a plugin during watch mode after
+    // a new compilation is triggered but before the compilation is actually started
+    this.hooks.watchRun.tapPromise("jerni-next-plugin", async (compiler) => {
+      const changed = this.modifiedFiles ?? [];
+      const removed = this.removedFiles ?? [];
+
+      const changedFiles = new Set([...changed, ...removed]);
+
+      if (changedFiles.size > 0 && jerniDeps.some((file) => changedFiles.has(file))) {
+        // reinitialize files to watch
+        jerniDeps = await getFilesToWatch(initializerAbsoluteFilePath);
+        // sync binary and clean start
+        scheduleCleanStartJerni(eventsFileAbsolutePath, sqliteFileAbsolutePath);
+      }
+
+      if (changedFiles.has(eventsFileAbsolutePath)) {
+        // todo: determine whether to clean start or not
+        // console.log("Events file changed, clean start");
+        // FIXME: db projections is not fired right away
+        scheduleCleanStartJerni(eventsFileAbsolutePath, sqliteFileAbsolutePath);
+      }
+    });
+  };
+}
---
+++ packages/jerni-next-dev-plugin/src/jerni-next-plugin/withJerniNextDev.mjs
@@ -0,0 +1,88 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import createJerniNextDevPlugin from "./createJerniNextPlugin.mjs";
+
+/**
+ * @param {import("next").NextConfig} nextConfig
+ * @param {Object} devConfig
+ * @param {string} devConfig.initializerPath
+ * @param {string} devConfig.eventsFile
+ * @returns {import("next").NextConfig}
+ */
+export default async function withJerniNextDev(nextConfig, { initializerPath, eventsFile }) {
+  const initializerAbsoluteFilePath = path.resolve(process.cwd(), initializerPath);
+  const eventsFileAbsolutePath = path.resolve(process.cwd(), eventsFile);
+  const sqliteFileAbsolutePath = path.resolve(process.cwd(), "events.sqlite");
+
+  const jerniDevPlugin = await createJerniNextDevPlugin({
+    initializerAbsoluteFilePath,
+    eventsFileAbsolutePath,
+    sqliteFileAbsolutePath,
+  });
+
+  const extendedConfig = {
+    ...nextConfig,
+
+    // in prior to nextjs v15, page router does not bundle external packages.
+    // -> load external packages at run time -> load original jerni-3
+    // in nextjs v15, page router does not bundle external packages
+    // https://nextjs.org/blog/next-15-rc#optimizing-bundling-of-external-packages-stable
+    // fixme: next version compatibility
+    // fixme: affect production build?
+    transpilePackages: Array.isArray(nextConfig.transpilePackages)
+      ? ["@jerni/jerni-3", ...nextConfig.transpilePackages]
+      : ["@jerni/jerni-3"],
+
+    webpack: (config, options) => {
+      // If nextConfig.webpack is a function, call it with config and options.
+      // Use this webpack config for further processing.
+      const webpackConfig = typeof nextConfig.webpack === "function" ? nextConfig.webpack(config, options) : {};
+
+      const { isServer, dev } = options;
+
+      if (isServer && dev) {
+        // Apply alias to resolve `@jerni/jerni-3` to `jerni-next-dev-plugin/jerni-next-dev`
+        if (webpackConfig.resolve.alias["@jerni/jerni-3$"]) {
+          // warn if alias is already defined
+          console.warn("[jerni-next-dev] Alias `@jerni/jerni-3$` is overridden by `jerni-next-dev-plugin`.");
+        }
+        webpackConfig.resolve.alias["@jerni/jerni-3$"] = "jerni-next-dev-plugin/jerni-next-dev";
+
+        // WORKAROUND: mark `better-sqlite3` as external package in order to load it at runtime instead of bundling it
+        // to overcome the issue that `bindings` package throws error
+        webpackConfig.externals = Array.isArray(webpackConfig.externals)
+          ? ["better-sqlite3", ...webpackConfig.externals]
+          : ["better-sqlite3"];
+
+        // Define the global variables dynamically
+        // IMPORTANT: there is another runtime value `globalThis.__JERNI_EVENTS__` in `jerni-next-dev`
+        webpackConfig.plugins.push(
+          new options.webpack.DefinePlugin({
+            "globalThis.__JERNI_EVENTS_FILE_PATH__": options.webpack.DefinePlugin.runtimeValue(
+              () => {
+                return JSON.stringify(eventsFileAbsolutePath);
+              },
+              {
+                fileDependencies: [eventsFileAbsolutePath], // watch events file
+              },
+            ),
+          }),
+        );
+        webpackConfig.plugins.push(
+          new options.webpack.DefinePlugin({
+            "globalThis.__JERNI_SQL_FILE_PATH__": options.webpack.DefinePlugin.runtimeValue(() => {
+              return JSON.stringify(sqliteFileAbsolutePath); // todo: should be internal in node_modules?
+            }),
+          }),
+        );
+
+        webpackConfig.plugins.push(jerniDevPlugin);
+      }
+
+      return webpackConfig;
+    },
+  };
+
+  return extendedConfig;
+}
