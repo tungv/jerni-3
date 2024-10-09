84ca1956c99d8bf18e0f4224f6e2a2b2fd76b2df
ttlong3103 <tranthanhlong3103@gmail.com>
2024-10-09 17:06:05 +0700

    clean start when dependencies change

--- packages/jerni-next-dev-plugin/src/jerni-next-plugin/createJerniNextPlugin.mjs
+++ packages/jerni-next-dev-plugin/src/jerni-next-plugin/createJerniNextPlugin.mjs
@@ -1,8 +1,7 @@
 import path from "node:path";
-import scheduleCleanStartJerni from "./cleanStartJerniDev.mjs";
 import getFilesToWatch from "./getFilesToWatch.mjs";
 import ensureMarkdownFileExists from "@jerni/jerni-3/dev-cli/ensureMarkdownFileExists";
-import { requestCleanStartForBootUp } from "./requestCleanStartForBootUp.mjs";
+import { requestCleanStartForBootUp } from "../lib/requestCleanStartForBootUp.mjs";
 
 export default async function createJerniNextPlugin(config) {
   const { initializerAbsoluteFilePath, eventsFileAbsolutePath, sqliteFileAbsolutePath } = config;
@@ -19,31 +18,20 @@ export default async function createJerniNextPlugin(config) {
     files.concat(eventsFileAbsolutePath),
   );
 
-  // sync binary and clean start
-  await scheduleCleanStartJerni(eventsFileAbsolutePath, sqliteFileAbsolutePath);
-
   return function jerniNextPlugin() {
     // watchRun hook executes a plugin during watch mode after
     // a new compilation is triggered but before the compilation is actually started
     this.hooks.watchRun.tapPromise("jerni-next-plugin", async (compiler) => {
       const changed = this.modifiedFiles ?? [];
       const removed = this.removedFiles ?? [];
-
       const changedFiles = new Set([...changed, ...removed]);
 
       if (changedFiles.size > 0 && jerniDeps.some((file) => changedFiles.has(file))) {
-        // todo: check this condition covers the below case `changedFiles.has(eventsFileAbsolutePath)`
+        // createJourney() will be reloaded hence it's ok to use requestCleanStartForBootUp()
+        requestCleanStartForBootUp();
         // reinitialize files to watch
         jerniDeps = await getFilesToWatch(initializerAbsoluteFilePath);
-        // sync binary and clean start
-        await scheduleCleanStartJerni(eventsFileAbsolutePath, sqliteFileAbsolutePath);
       }
-
-/**
- * Jerni dev will read `globalThis.__JERNI_BOOTED_UP__` to determine whether to
- * make a clean start when `createJourney()` invoked as a boot up to sync markdown file,
- * sqlite file and data in stores.
- */
-function requestCleanStartForBootUp() {
-  globalThis.__JERNI_BOOTED_UP__ = true;
+    });
+  };
 }
