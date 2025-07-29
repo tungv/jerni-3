04e0048621fc1d9d3bf6cd3c030ee827655c54d4
Quan Vo <votrungquan99@gmail.com>
2025-07-29 11:05:26 +0700

    refactor: only check if there are any clean start request instead of reading large event file

--- packages/jerni-next-dev-plugin/src/jerni-next-dev/shouldCleanStart.ts
+++ packages/jerni-next-dev-plugin/src/jerni-next-dev/shouldCleanStart.ts
@@ -1,10 +1,11 @@
-import readEventsFromMarkdown from "./readEventsFromMarkdown";
+import { hasCleanStartRequest } from "./cleanStartRequestHelpers";
+import { getDevFilesDir } from "./getDevFilesUtils";
 
+/**
+ * This function should only be called in the plugin code, not from the user's code
+ * Check if there is a clean start request
+ */
 export default async function shouldCleanStart(): Promise<boolean> {
-  // @ts-expect-error
-  const absoluteEventsFilePath = globalThis.__JERNI_EVENTS_FILE_PATH__;
-  const { realChecksum, fileChecksum } = await readEventsFromMarkdown(absoluteEventsFilePath);
-
-  // if checksum and events not match, need to clean start
-  return realChecksum !== fileChecksum;
+  const devFilesDir = getDevFilesDir();
+  return hasCleanStartRequest(devFilesDir);
 }
