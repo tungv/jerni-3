a907cb6247d42f5e8367dd34b89d4a14c66fa763
Quan Vo <votrungquan99@gmail.com>
2025-07-30 15:11:54 +0700

    chore: update better-sqlite3 dependency to version 12.2.0 and add new utility function for event file path

--- packages/jerni-next-dev-plugin/src/jerni-next-dev/getDevFilesUtils.ts
+++ packages/jerni-next-dev-plugin/src/jerni-next-dev/getDevFilesUtils.ts
@@ -2,3 +2,8 @@ export function getDevFilesDir(): string {
   // @ts-expect-error
   return globalThis.__JERNI_DEV_FILES_DIR__;
 }
+
+export function getEventsFilePath(): string {
+  // @ts-expect-error
+  return globalThis.__JERNI_EVENTS_FILE_PATH__;
+}
