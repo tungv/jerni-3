5a4c88b56fe54116f82d9f4b54e6dda3c0532805
Quan Vo <votrungquan99@gmail.com>
2025-07-30 14:55:42 +0700

    feat: have different clean start check for commit and get reader
    - when checking for clean start before commit, also check if the checksum mismatch

--- packages/jerni-next-dev-plugin/src/jerni-next-dev/shouldCleanStart.ts
+++ packages/jerni-next-dev-plugin/src/jerni-next-dev/shouldCleanStart.ts
@@ -1,11 +1,28 @@
 import { hasCleanStartRequest } from "./cleanStartRequestHelpers";
-import { getDevFilesDir } from "./getDevFilesUtils";
+import { getDevFilesDir, getEventsFilePath } from "./getDevFilesUtils";
+import readEventsFromMarkdown from "./readEventsFromMarkdown";
 
 /**
  * This function should only be called in the plugin code, not from the user's code
- * Check if there is a clean start request
+ * Check if there is a clean start request before reading data
  */
-export default async function shouldCleanStart(): Promise<boolean> {
+export async function shouldCleanStartForReader(): Promise<boolean> {
   const devFilesDir = getDevFilesDir();
   return hasCleanStartRequest(devFilesDir);
+}
+
+/**
+ * Check if there is a clean start request before committing
+ * Also need to check if the checksum of the events file are mismatch to clean start
+ * Otherwise, the new event will not be projected correctly
+ */
+export async function shouldCleanStartForCommit(): Promise<boolean> {
+  const devFilesDir = getDevFilesDir();
+  const cleanStartRequested = await hasCleanStartRequest(devFilesDir);
+
+  const eventsFilePath = getEventsFilePath();
+  const { fileChecksum, realChecksum } = await readEventsFromMarkdown(eventsFilePath);
+  const checksumMismatch = fileChecksum !== realChecksum;
+
+  return cleanStartRequested || checksumMismatch;
 }
